ac58632eb500bb8e8240f3a240179e47216c0197
Carlos Berdugo <carlos.berdugo2@halliburton.com>
2023-04-25 14:04:00 -0500

    Added barrier selection

---
+++ src/common/interfaces/BarrierElementData.ts
@@ -0,0 +1,32 @@
+export interface BarrierElementData{
+    well_id:string;
+    wellbore_id:string;
+    barrier_envelope_id:string;
+    barrier_element_id:string;
+    wellhead_id?:string;
+    wellhead_comp_id?:string;
+    wellhead_outlet_id?:string;
+    assembly_id?:string;
+    assembly_comp_id?:string;
+    is_closed?:boolean;
+    is_closed_top?:boolean;
+    is_closed_bottom?:boolean;
+    include_seals?:boolean;
+    cement_job_id?:string;
+    cement_stage_id?:string;
+    top_depth?:string;
+    base_depth?:string;
+    wellbore_formation_id?:string;
+    completion_id?:string;
+    completion_fluid_id?:string;
+    name?:string;
+    wellhead_hanger_id?:string;
+    barrier_diagram_id?:string;
+    component_ovality?:number;
+    component_wearing?:number;
+    element_type?:string;
+    evaluation?:string;
+    evaluation_comments?:string;
+    ref_id:string;
+    scenario_id:string;
+}
---
+++ src/common/interfaces/DTO/BarriersModifyDTO.ts
@@ -0,0 +1,13 @@
+export interface BarriersModifyDTO {
+  barrier_modify_data: BarrierModifyData[];
+  scenario_id: string;
+  wellbore_id: string;
+  well_id: string;
+  schematic_date: Date;
+}
+
+export interface BarrierModifyData {
+  barrier: string;
+  elementType: string;
+  eventRefId: string;
+}
